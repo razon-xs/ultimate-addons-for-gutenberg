479c22f49acfd04ef265f081a463caff66729353
Tushar Imran <tusharimran9909@gmail.com>
2022-12-29 22:56:03 +0600

    Separator Block - Added Editor Block Icon

--- src/blocks/separator/block.js
+++ src/blocks/separator/block.js
@@ -13,9 +13,8 @@ import { registerBlockType } from '@wordpress/blocks';
 registerBlockType( 'uagb/separator', {
 	title: __( 'Separator', 'ultimate-addons-for-gutenberg' ),
 	description: __( 'Add Block Separator', 'ultimate-addons-for-gutenberg' ),
-	icon: '',
+	icon: UAGB_Block_Icons.separator,
 	category: uagb_blocks_info.category,
-	icon: UAGB_Block_Icons.advanced_heading,
 	keywords: [
 		__( 'divider', 'ultimate-addons-for-gutenberg' ),
 		__( 'separator', 'ultimate-addons-for-gutenberg' ),
