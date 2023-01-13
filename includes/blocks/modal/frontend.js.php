2a5c9ee0aa3612f560c6db1aa18053d6a9d7b006
Abhishek Rijal <avee.ws@gmail.com>
2023-01-13 08:28:06 +0545

    Added args for init

--- includes/blocks/modal/frontend.js.php
+++ includes/blocks/modal/frontend.js.php
@@ -8,10 +8,15 @@
  */
 
 $selector = '.uagb-block-' . $id;
+$args     = array(
+	'modalTrigger' => $attr['modalTrigger'],
+	'cssClass'     => $attr['cssClass'],
+	'cssID'        => $attr['cssID']
+);
 ob_start();
 ?>
 	window.addEventListener( 'DOMContentLoaded', function() {
-		UAGBModal.init( '<?php echo esc_attr( $selector ); ?>' );
+		UAGBModal.init( '<?php echo esc_attr( $selector ); ?>', '', false, '<?php echo json_encode( $args ) ?>' );
 	});
 <?php
 return ob_get_clean();
