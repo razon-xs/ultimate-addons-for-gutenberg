a1e84cfbdada44824cba5b052ede2880ae5e819e
Supriya Jadhav <supriyaj@bsf.io>
2023-02-06 10:51:40 +0530

    Added icon SVG's and variation designs

---
+++ src/blocks/new-block/icons.js
@@ -0,0 +1,74 @@
+/**
+ * Custom icons
+ */
+const rowIcons = {
+	'style-1' : ( //Done
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="0.7" y="4.7" width="22.6" height="14.6" rx="1.3" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M2.80957 14.0093V9.72326C2.80957 8.37063 3.90609 7.27411 5.25872 7.27411H9.54474C10.8974 7.27411 11.9939 8.37063 11.9939 9.72326V14.0093M2.80957 14.0093C2.80957 15.3619 3.90609 16.4584 5.25872 16.4584H9.54474C10.8974 16.4584 11.9939 15.3619 11.9939 14.0093M2.80957 14.0093L5.19356 12.0226C5.97084 11.3749 7.06642 11.2718 7.95089 11.7632L11.9939 14.0093" stroke="#1E293B" strokeWidth="1.4"/>
+			<path d="M14.0078 9.99866H21.4055" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M14.0078 7.75214H17.7064" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M13.4414 16.0628C13.4414 15.6301 13.7922 15.2793 14.225 15.2793H20.9665C21.3992 15.2793 21.75 15.6301 21.75 16.0628C21.75 16.4956 21.3992 16.8464 20.9665 16.8464H14.225C13.7922 16.8464 13.4414 16.4956 13.4414 16.0628Z" stroke="#1E293B" strokeWidth="0.5" strokeLinecap="round"/>
+			<path d="M13.8914 13.367C13.8914 13.1828 14.0407 13.0335 14.225 13.0335H20.9665C21.1507 13.0335 21.3 13.1828 21.3 13.367C21.3 13.5513 21.1507 13.7006 20.9665 13.7006H14.225C14.0407 13.7006 13.8914 13.5513 13.8914 13.367Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+		</svg>
+	),
+	'style-2' : ( //Done
+		<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_1760_7689)">
+			<rect x="1.59307" y="4.7" width="22.6" height="14.6" rx="1.3" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M3.70264 14.0093V9.72326C3.70264 8.37063 4.79916 7.27411 6.15179 7.27411H10.4378C11.7904 7.27411 12.887 8.37063 12.887 9.72326V14.0093M3.70264 14.0093C3.70264 15.3619 4.79916 16.4584 6.15179 16.4584H10.4378C11.7904 16.4584 12.887 15.3619 12.887 14.0093M3.70264 14.0093L6.08663 12.0226C6.86391 11.3749 7.95949 11.2718 8.84395 11.7632L12.887 14.0093" stroke="#1E293B" strokeWidth="1.4"/>
+			<path d="M14.9009 12.6273H22.0754" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M14.9009 14.6599H22.0754" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M14.893 9.725C14.893 9.42174 15.1224 9.17589 15.4055 9.17589C15.6885 9.17589 15.918 9.42174 15.918 9.725C15.918 10.0283 15.6885 10.2741 15.4055 10.2741C15.1224 10.2741 14.893 10.0283 14.893 9.725ZM14.893 9.725C14.893 9.725 14.893 8.90134 15.6617 8.35223" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
+			<path d="M17.5746 9.725C17.5746 9.42174 17.8041 9.17589 18.0871 9.17589C18.3702 9.17589 18.5996 9.42174 18.5996 9.725C18.5996 10.0283 18.3702 10.2741 18.0871 10.2741C17.8041 10.2741 17.5746 10.0283 17.5746 9.725ZM17.5746 9.725C17.5746 9.725 17.5746 8.90134 18.3434 8.35223" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"/>
+			</g>
+			<defs>
+			<clipPath id="clip0_1760_7689">
+				<rect width="24" height="24" fill="white" transform="translate(0.893066)"/>
+			</clipPath>
+			</defs>
+		</svg>
+	),
+	'style-3' : ( //Done
+		<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clipPath="url(#clip0_1760_7675)">
+			<rect x="20.1931" y="0.7" width="22.6" height="14.6" rx="1.3" transform="rotate(90 20.1931 0.7)" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M9.10889 4H16.6772" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M12.0352 8H17.0352" stroke="#1E293B" strokeLinecap="round"/>
+			<path d="M12.0352 12H17.0352" stroke="#1E293B" strokeLinecap="round"/>
+			<path d="M12.0352 16H17.0352" stroke="#1E293B" strokeLinecap="round"/>
+			<path d="M10.051 8.00001C10.051 8.1657 9.91667 8.30002 9.75098 8.30002C9.58529 8.30002 9.45098 8.1657 9.45098 8.00001C9.45098 7.83432 9.58529 7.7 9.75098 7.7C9.91667 7.7 10.051 7.83432 10.051 8.00001Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M10.051 12C10.051 12.1657 9.91667 12.3 9.75098 12.3C9.58529 12.3 9.45098 12.1657 9.45098 12C9.45098 11.8343 9.58529 11.7 9.75098 11.7C9.91667 11.7 10.051 11.8343 10.051 12Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M10.051 16C10.051 16.1657 9.91667 16.3 9.75098 16.3C9.58529 16.3 9.45098 16.1657 9.45098 16C9.45098 15.8343 9.58529 15.7 9.75098 15.7C9.91667 15.7 10.051 15.8343 10.051 16Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M9.18877 20.1072C9.18877 19.923 9.3381 19.7737 9.52231 19.7737H16.2638C16.448 19.7737 16.5974 19.923 16.5974 20.1072C16.5974 20.2914 16.448 20.4408 16.2638 20.4408H9.52231C9.3381 20.4408 9.18877 20.2914 9.18877 20.1072Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			</g>
+			<defs>
+			<clipPath id="clip0_1760_7675">
+				<rect width="24" height="24" fill="white" transform="translate(0.893066)"/>
+			</clipPath>
+			</defs>
+		</svg>
+	
+	),
+	'style-4' : ( //Done
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M2.77197 8.79663H21.3374" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M2.77197 11.3983H21.3374" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M6.22266 14H17.7773" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M5.62207 5.03101H18.4869" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M14.7041 19.2678H19.2441" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M5.45586 19.2678C5.45586 18.9542 5.71008 18.7 6.02367 18.7H11.8406C12.1542 18.7 12.4084 18.9542 12.4084 19.2678C12.4084 19.5814 12.1542 19.8356 11.8406 19.8356H6.02367C5.71008 19.8356 5.45586 19.5814 5.45586 19.2678Z" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+		</svg>	
+	),
+	'style-5' : ( //Done
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="4.7" y="0.7" width="14.6" height="22.6" rx="1.3" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M7.11768 10.4167V5.85968C7.11768 4.42153 8.28353 3.25568 9.72168 3.25568H14.2787C15.7168 3.25568 16.8827 4.42153 16.8827 5.85968V10.4167M7.11768 10.4167C7.11768 11.8548 8.28353 13.0207 9.72168 13.0207H14.2787C15.7168 13.0207 16.8827 11.8548 16.8827 10.4167M7.11768 10.4167L9.6524 8.30442C10.4788 7.61573 11.6437 7.50612 12.5841 8.02856L16.8827 10.4167" stroke="#1E293B" strokeWidth="1.4"/>
+			<path d="M7.12109 15.521L15.9998 15.4341" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M7.12109 17.5665H16" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+			<path d="M7.12109 20.3475H10.8716" stroke="#1E293B" strokeWidth="1.4" strokeLinecap="round"/>
+		</svg>
+	),
+};
+
+export default rowIcons;
--- src/blocks/new-block/variations.js
+++ src/blocks/new-block/variations.js
@@ -3,7 +3,7 @@
  */
 
 import { __ } from '@wordpress/i18n';
-import UAGB_Block_Icons from '@Controls/block-icons';
+import icons from './icons';
 /**
  * Template option choices for predefined form layouts.
  *
@@ -12,21 +12,39 @@ import UAGB_Block_Icons from '@Controls/block-icons';
  */
 const variations = [
 	{
-		name: 'info-box',
-		icon: UAGB_Block_Icons.info_box,
+		name: 'style-1',
+		icon: icons['style-1'],
 		title: __( 'Style 1', 'ultimate-addons-for-gutenberg' ),
-		attributes: {},
+		attributes: {
+			backgroundType: 'color',
+			backgroundColor: '#ffffff',
+			blockBorderTopLeftRadius: 4,
+			blockBorderTopRightRadius: 4,
+			blockBorderBottomRightRadius: 4,
+			blockBorderBottomLeftRadius: 4,
+			boxShadowColor: '#00000021',
+			boxShadowHOffset: 0,
+			boxShadowVOffset: 12,
+			boxShadowBlur: 16,
+			boxShadowSpread: -4,
+			boxShadowPosition: 'outset',
+			topPaddingDesktop: 24,
+			bottomPaddingDesktop: 24,
+			leftPaddingDesktop: 24,
+			rightPaddingDesktop: 24,
+		},
 		isDefault: true,
 		innerBlocks: [
 			[ 'uagb/advanced-heading', { headingAlign: '',blockBottomPadding:0, headingDescToggle:true, headingDesc: 'Prefix',subHeadSpace:0 }],
 			[ 'uagb/advanced-heading', { headingAlign: '', headingTitleToggle: false, headingDescToggle:true,headingDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' } ],
-			[ 'uagb/buttons', { btn_count: 1, align: '' }],
+			[ 'uagb/advanced-heading', { headingAlign: '',blockBottomPadding:0, headingDescToggle:true, headingDesc: 'Prefix',subHeadSpace:0 }],
+			[ 'uagb/advanced-heading', { headingAlign: '', headingTitleToggle: false, headingDescToggle:true,headingDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' } ],  
 		],
 		scope: [ 'block' ],
 	},
 	{
-		name: 'price-list',
-		icon: UAGB_Block_Icons.icon_list,
+		name: 'style-2',
+		icon: icons['style-2'],
 		title: __( 'Style 2', 'ultimate-addons-for-gutenberg' ),
 		attributes: {},
 		isDefault: true,
@@ -39,9 +57,23 @@ const variations = [
 		scope: [ 'block' ],
 	},
 	{
-		name: 'testimonial',
-		icon: UAGB_Block_Icons.testimonial,
+		name: 'style-3',
+		icon: icons['style-3'],
 		title: __( 'Style 3', 'ultimate-addons-for-gutenberg' ),
+		attributes: {},
+		isDefault: true,
+		innerBlocks: [
+			[ 'uagb/image', { align:'', width: 100, height: 100, url:`${ uagb_blocks_info.uagb_url }/admin/assets/images/uag-placeholder.svg`,maskShape: 'circle'}],
+			[ 'uagb/advanced-heading', { headingAlign: '', blockTopPadding:0, headingDescToggle:true,headingDesc: 'Designation', headingTitle: 'Author Name' } ],
+			[ 'uagb/social-share', { align: '' } ],
+			[ 'uagb/advanced-heading', { headingAlign: '', headingTitleToggle: false, headingDescToggle:true,headingDesc: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' } ],
+		],
+		scope: [ 'block' ],
+	},
+	{
+		name: 'style-4',
+		icon: icons['style-4'],
+		title: __( 'Style 4', 'ultimate-addons-for-gutenberg' ),
 		attributes: {},
 		isDefault: true,
 		innerBlocks: [
@@ -51,9 +83,9 @@ const variations = [
 		scope: [ 'block' ],
 	},
 	{
-		name: 'team',
-		icon: UAGB_Block_Icons.team,
-		title: __( 'Style 4', 'ultimate-addons-for-gutenberg' ),
+		name: 'style-5',
+		icon: icons['style-5'],
+		title: __( 'Style 5', 'ultimate-addons-for-gutenberg' ),
 		attributes: {},
 		isDefault: true,
 		innerBlocks: [
