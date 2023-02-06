1756fea2c32a0613d2739c070f987ef370fd6eb9
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2023-02-06 14:26:16 +0530

    phpcbf fix

--- classes/class-uagb-block-helper.php
+++ classes/class-uagb-block-helper.php
@@ -1595,7 +1595,7 @@ if ( ! class_exists( 'UAGB_Block_Helper' ) ) {
 		 */
 		public static function uag_generate_border_css( $attr, $prefix, $device = 'desktop' ) {
 			$gen_border_css = array();
-			//ucfirst function is used to tranform text into first letter capital
+			// ucfirst function is used to tranform text into first letter capital
 			$device = $device === 'desktop' ? '' : ucfirst( $device );
 			if ( 'none' !== $attr[ $prefix . 'BorderStyle' ] && ! empty( $attr[ $prefix . 'BorderStyle' ] ) ) {
 				$gen_border_css['border-top-width']    = UAGB_Helper::get_css_value( $attr[ "{$prefix}BorderTopWidth{$device}" ], 'px' );
