ad7c5cba37c6b1dcc1b6a2c9503848ce27a306e4
Antariksh Patre <antarikshpatre@gmail.com>
2023-02-08 22:35:15 +0530

    SPCTR-2215 Countdown Pro - Admin core build

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '193c861ba1385f9827a64ed8598e487e');
-=======
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '2cc042de7228e3fd82e05330ffcc6a34');
->>>>>>> 633a9620b96c0a281136f9e610eba6759cb3a6fc
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd4abdae3cf55938a858d960ebf19ff20');
