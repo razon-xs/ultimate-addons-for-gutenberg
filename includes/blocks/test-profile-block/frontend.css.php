918c79440210fc3b17c753f529508fe336b1a7ab
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-02-13 13:23:40 +0530

    fixed css file error

--- includes/blocks/test-profile-block/frontend.css.php
+++ includes/blocks/test-profile-block/frontend.css.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Frontend CSS & Google Fonts loading File.
+ *
+ * @since x.x.x
+ *
+ * @package uagb
+ */
 
 $selectors = array(
     ' .uagb-user__title'=> array(
@@ -6,17 +13,17 @@ $selectors = array(
     ),
 );
 $Mobselectors = array(
-    ' .uagb-user__title'=>array(
+    ' .uagb-user__title'=> array(
         'color'=> $attr['titleColor'],
     ),
 );
 
 $Tabselectors = array(
-    ' .uagb-user__title'=>array(
+    ' .uagb-user__title'=> array(
         'color'=> $attr['titleColor'],
     ),
 );
-$combined_selectors                                      = array(
+$combined_selectors   = array(
 	'desktop' => $selectors,
 	'tablet'  => $Tabselectors,
 	'mobile'  => $Mobselectors,
