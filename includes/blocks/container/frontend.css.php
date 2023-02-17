c279cded284c18a01d5919ee5ace599dabbfb3e2
Supriya Jadhav <supriyaj@bsf.io>
2023-02-17 10:23:31 +0530

    Resolved merge conflict

--- includes/blocks/container/frontend.css.php
+++ includes/blocks/container/frontend.css.php
@@ -130,7 +130,7 @@ $background_video_opacity_value = ( isset( $attr['backgroundVideoOpacity'] ) && 
 
 $selectors = array(
 	'.uagb-block-' . $id . '.wp-block-uagb-container' => array(
-		'color'        => $attr['textColor'],
+		'color' => $attr['textColor'],
 	),
 	'.uagb-block-' . $id . ' a'                       => array(
 		'color' => $attr['linkColor'],
