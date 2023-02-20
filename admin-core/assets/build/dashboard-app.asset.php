6b6577f6280da64e498cd7916b02c081807052bb
Zane Vaz <zanev@bsf.io>
2023-02-20 19:04:48 +0530

    SPCTR-556: Resolved PR Comments

--- admin-core/assets/build/dashboard-app.asset.php
+++ admin-core/assets/build/dashboard-app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '0a8ed84a9d0c79c55c45');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-escape-html', 'wp-i18n'), 'version' => '7377153ef0dbc640871d');
--- classes/class-uagb-block.php
+++ classes/class-uagb-block.php
@@ -49,16 +49,12 @@ if ( ! class_exists( 'UAGB_Block' ) ) {
 		 * @since 2.1.0
 		 * @param string $block_file Block File Path.
 		 */
-		public function register( $block_file, $is_pro = NULL ) {
+		public function register( $block_file ) {
 
 			$block_slug = '';
 			$block_data = array();
 
 			include $block_file;
-
-			if( $is_pro ) {
-				die( file_exists( $block_file ) );
-			}
 			
 			if ( ! empty( $block_slug ) && ! empty( $block_data ) ) {
 				self::$blocks[ $block_slug ] = $block_data;
