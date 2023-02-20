c334ee6f11595b96ebdb85d6cc24e8e643ebc1fe
Antariksh Patre <antarikshpatre@gmail.com>
2023-02-20 15:04:01 +0530

    Countdown - Presets, Render - Converted all indentations to tabs

--- src/blocks/countdown/render.js
+++ src/blocks/countdown/render.js
@@ -7,7 +7,7 @@ import CountdownBox from './components/CountdownBox';
 
 const Render = ( props ) => {
 
-    // Add and remove the CSS on the drop and remove of the component.
+	// Add and remove the CSS on the drop and remove of the component.
 	useLayoutEffect( () => {
 		styles.use();
 		return () => {
@@ -15,38 +15,38 @@ const Render = ( props ) => {
 		};
 	}, [] );
 
-    const countdownRef = props.countdownRef;
+	const countdownRef = props.countdownRef;
 
 	props = props.parentProps;
 
-    const deviceType = useDeviceType();
+	const deviceType = useDeviceType();
 	const { attributes } = props;
 
-    const {
-        block_id,
-        showLabels,
-        labelDays,
-        labelHours,
-        labelMinutes,
-        labelSeconds,
-    } = attributes;
+	const {
+		block_id,
+		showLabels,
+		labelDays,
+		labelHours,
+		labelMinutes,
+		labelSeconds,
+	} = attributes;
 
-    return(
-        <div
-            className={ classnames(
-                props.className,
-                `uagb-editor-preview-mode-${ deviceType.toLowerCase() }`,
-                `uagb-block-${ block_id }`,
-                'wp-block-uagb-countdown',
-            ) }
-            ref = { countdownRef }
-        >
-            <CountdownBox unitType='days' showLabels={ showLabels } label={ labelDays } />
-            <CountdownBox unitType='hours' showLabels={ showLabels } label={ labelHours } />
-            <CountdownBox unitType='minutes' showLabels={ showLabels } label={ labelMinutes } />
-            <CountdownBox unitType='seconds' showLabels={ showLabels } label={ labelSeconds } />
-        </div>
-    );
+	return(
+		<div
+			className={ classnames(
+				props.className,
+				`uagb-editor-preview-mode-${ deviceType.toLowerCase() }`,
+				`uagb-block-${ block_id }`,
+				'wp-block-uagb-countdown',
+			) }
+			ref = { countdownRef }
+		>
+			<CountdownBox unitType='days' showLabels={ showLabels } label={ labelDays } />
+			<CountdownBox unitType='hours' showLabels={ showLabels } label={ labelHours } />
+			<CountdownBox unitType='minutes' showLabels={ showLabels } label={ labelMinutes } />
+			<CountdownBox unitType='seconds' showLabels={ showLabels } label={ labelSeconds } />
+		</div>
+	);
 };
 
 export default Render;
